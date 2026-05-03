652943c26ef862b250d919c72244dca584f59e01
Shashank <shashank_pandey93@yahoo.com>
2026-05-03 14:10:39 +0200

    Add the training code to the self-attention code

--- v2.py
+++ v2.py
@@ -6,13 +6,62 @@ from symtable import Class
 
 #hyperramameters
 batch_size = 64 # how many independent sequences will we process in parallel?
-block_size = 256 # what is the maximum context length for predictions?
+block_size = 8 # what is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 500
 learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_embd = 32
+batch_size = 4
+context_length = 8
+
+torch.manual_seed(1337)
+with open('dataset/tiny.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+# Tiny Shakespeare dataset is used because it is short enought to train on the local computer but long enough i.e. 1 Million characters that it will be hard for a human to cheat/augment the model by providing the answers or regex.
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+
+stoi = {ch:i for i, ch in enumerate(chars)}
+itos = {i:ch for i, ch in enumerate(chars)}
+
+encoding = lambda s: [stoi[c] for c in s] # Character -> Integer
+decoding = lambda l: ''.join(itos[i] for i in l) # Integer -> Character, this is only a checking step
+
+print(encoding("ADD"))
+print(decoding(encoding("ADD")))
+
+data = torch.tensor(encoding(text), dtype=torch.long)
+print(data.shape, data.dtype)
+
+n = int(0.9*(len(data)))
+train_data = data[:n]
+test_data = data[n:]
+
+context_length = 8
+train_data[:context_length+1]
+
+x = train_data[:context_length]
+y = train_data[1:context_length+1]
+
+for t in range(context_length):
+    context = x[:t+1]
+    target = y[t]
+    #print(f'When the input is {context} the target is {target}')
+
+
+
+# _______________________________
+def get_batch(split):
+    data = train_data if split == 'train' else test_data
+    offset = torch.randint(len(data) - context_length, (batch_size,))
+    x = torch.stack([data[i:i+context_length] for i in offset])
+    y = torch.stack([data[i+1:i+context_length+1] for i in offset])
+    x,y = x.to(device), y.to(device)
+    return x, y
+
+xb,yb = get_batch('train')
 
 class Head(nn.Module):
     """ one head of self-attention """
@@ -59,13 +108,14 @@ class BigramLanguageModel(nn.Module):
         x = self.sa_head(x)
         logits = self.lm_head(x) # (B,T,vocab_size)
 
-        if targets is not None:
+        if targets is None:
             loss = None
         else:
             B,T,C = logits.shape
             logits = logits.view(B*T, C)
             targets = targets.view(B*T)
             loss = F.cross_entropy(logits, targets)
+        return logits, loss
     
     def generate(self, idx, max_new_tokens):
         # idx is (B,T) array of indices in the current context
@@ -88,3 +138,25 @@ class BigramLanguageModel(nn.Module):
             # append sampled index to the running sequence
             idx = torch.cat((idx, idx_next), dim=1) # (B,T+1)
         return idx
+    
+
+m = BigramLanguageModel()
+logits, loss = m(xb, yb)
+print(logits.shape)
+print('Loss is ', loss)
+
+optimiszer = torch.optim.AdamW(m.parameters(), lr=1e-3)
+print(optimiszer)
+
+for steps in range(10000):
+    # sample a batch of data
+    xb, yb = get_batch('train')
+
+    # evaluate the loss
+    logits, loss = m(xb, yb)
+
+    # optimize the model
+    optimiszer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimiszer.step()
+print(f'train loss {loss.item()}')
