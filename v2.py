d3d72be8abe1d5ea622115f7cc5482d43d1218bd
Shashank <shashank_pandey93@yahoo.com>
2026-05-03 15:11:56 +0200

    Adjust the hyperparameter for scale

--- v2.py
+++ v2.py
@@ -6,15 +6,17 @@ from symtable import Class
 
 #hyperramameters
 batch_size = 64 # how many independent sequences will we process in parallel?
-block_size = 8 # what is the maximum context length for predictions?
+block_size = 256 # what is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 500
-learning_rate = 1e-3
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 32
-batch_size = 4
-context_length = 8
+n_embd = 384
+context_length = 256
+n_layer = 6
+n_head = 6
+dropout = 0.2
 
 torch.manual_seed(1337)
 with open('dataset/tiny.txt', 'r', encoding='utf-8') as f:
