2376ffc6ede01fa61001fdf5a63377bbd75df5a8
Shashank <shashank_pandey93@yahoo.com>
2026-05-03 13:46:29 +0200

    Add the class for training Bigram nodes

--- v2.py
+++ v2.py
@@ -1,9 +1,18 @@
-from train import BigramLanguageModel
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from symtable import Class
 
-
-
-
+#hyperramameters
+batch_size = 64 # how many independent sequences will we process in parallel?
+block_size = 256 # what is the maximum context length for predictions?
+max_iters = 5000
+eval_interval = 500
+learning_rate = 1e-3
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters = 200
+n_embd = 32
 
 class Head(nn.Module):
     """ one head of self-attention """
@@ -29,10 +38,53 @@ class Head(nn.Module):
         out = wei @ v         # (B,T,T) @ (B,T,head_size) -> (B,T,head_size)
         return out
     
-    Class BigramLanguageModel(nn.Module):
+
+class BigramLanguageModel(nn.Module):
 
     def __init__(self):
         super().__init__()
-        self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
-        self.position_embedding_table = nn.Embedding(block_size, n_embed)
-        self.lm_head = nn.Linear(n_embed, vocab_size)
+        # each token directly reads off the logit for the next token from a lookup table.
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.sa_head = Head(n_embd)
+        self.lm_head = nn.Linear(n_embd, vocab_size)
+
+    def forward(self, idx, targets=None):
+        B,T = idx.shape
+
+        # idx and targets are both (B,T) tensor of integers
+        token_emb = self.token_embedding_table(idx)   # (B,T,C)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=idx.device)) # (T,C)
+        x = token_emb + pos_emb  # (B,T,C)
+        x = self.sa_head(x)
+        logits = self.lm_head(x) # (B,T,vocab_size)
+
+        if targets is not None:
+            loss = None
+        else:
+            B,T,C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+    
+    def generate(self, idx, max_new_tokens):
+        # idx is (B,T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:,-block_size:] # (B,T)
+
+            # get the predictions
+            logits, loss = self(idx)
+
+            # focus only on the last time step
+            logits = logits[:, -1, :] # becomes (B,C)
+
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1) # (B,vocab_size)
+
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B,1)
+
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B,T+1)
+        return idx
